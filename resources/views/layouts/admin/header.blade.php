ff6dc669a3bea1421e18992f236b93f548aedcc1
Reyvi Rahman <reyvirahman@gmail.com>
2026-03-10 09:14:32 +0700

    Change sidebar logo

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -10,7 +10,7 @@
 
             <!-- Topbar Brand Logo -->
             <a href="index.html" class="md:hidden flex">
-                <img src="{{ asset('assets/images/logo-sm.png') }}" class="h-5" alt="Small logo">
+                <img src="{{ asset('assets/images/logo-ukbi.png') }}" class="h-5" alt="Small logo">
             </a>
 
             <!-- Topbar Search -->
